5fc94c104dec9f400a8a7d7fb9dd951d05790200
RobertoOrtegaOrtega <roo0002@alu.ubu.es>
2017-02-28 12:23:03 +0100

    //TODO Transform the code to be used safely in a concurrent context.

--- 2017PCTR_M202/src/p01/Ball.java
+++ 2017PCTR_M202/src/p01/Ball.java
@@ -2,7 +2,7 @@ package p01;
 
 import java.awt.Image;
 import javax.swing.ImageIcon;
-//TODO Transform the code to be used safely in a concurrent context.  
+ 
 public class Ball {
         
 	private String Ball = "/Ball.png"; 
@@ -20,7 +20,7 @@ public class Ball {
 		fi =  Math.random() * Math.PI * 2;
 	}
 
-	public void move() {
+	public synchronized void move() {
 		v = v*Math.exp(-v/1000);
 		dx = v*Math.cos(fi);
 		dy = v*Math.sin(fi);
@@ -34,7 +34,7 @@ public class Ball {
 		postCondition(x,y);
 	}
 
-	public void reflect() {
+	public synchronized void reflect() {
 		if (Math.abs(x + 32 - Board.RIGHTBOARD) <  Math.abs(dx)) {
 			fi = Math.PI - fi;
 		}
